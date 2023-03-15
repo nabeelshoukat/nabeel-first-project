c8a4c36968a1b059fc44b3bf24b05b3499c7d1be
Nabeel Shaukat <66872292+nabeelshoukat@users.noreply.github.com>
2023-03-15 12:04:12 +0500

    update set

--- Sheraz_aziz_project/settings.py
+++ Sheraz_aziz_project/settings.py
@@ -100,7 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -112,10 +111,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     # maybe other here
     'app.context_processors.common_data',
 )
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'Project_management/static', )
-
 MEDIA_ROOT = '/Project_management/static/media/'
 
 MEDIA_URL = '/media/'
